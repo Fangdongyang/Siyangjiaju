a381481ad68e2fe58878eab8c92ca4d2eb927acb
Fangdongyang <fangdongyang0858@gmail.com>
2018-12-17 18:35:22 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,83 @@
+//index.js
+const app = getApp()
+Page({
+  data: {
+    imgUrls: [
+      '/images/swiperPics/1.jpg',
+      '/images/swiperPics/2.jpg',
+      '/images/swiperPics/3.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+
+
+    routers:[
+      {
+        name:'移门室内门',
+        url:'/pages/woods/woods',
+        icon:'/images/icons/doors.png'
+      },
+      {
+        name: '纱窗阳光房',
+        url: '/pages/windows/windows',
+        icon: '/images/icons/windows.png'
+      },
+      {
+        name: '瓷砖',
+        url: '/pages/tiles/tiles',
+        icon: '/images/icons/tiles.png'
+      },
+      {
+        name: '厨卫',
+        url: '/pages/kitchen/kitchen',
+        icon: '/images/icons/toilets.png'
+      },
+      {
+        name: '家具',
+        url: '/pages/closets/closets',
+        icon: '/images/icons/closet.png'
+      },
+      {
+        name: '水电材料',
+        url: '/pages/electrocity/electrocity',
+        icon: '/images/icons/electrocity.png'
+      },
+      {
+        name: '水泥黄沙',
+        url: '/pages/concrete/concrete',
+        icon: '/images/icons/concrete.png'
+      },
+      {
+        name: '墙面油漆',
+        url: '/pages/paint/paint',
+        icon: '/images/icons/paint.png'
+      },
+      {
+        name: '窗帘布艺',
+        url: '/pages/curtains/curtains',
+        icon: '/images/icons/curtains.png'
+      },
+      {
+        name: '家政服务',
+        url: '/pages/homemaids/homemaids',
+        icon: '/images/icons/homemaids.png'
+      },
+      {
+        name: '家电',
+        url: '/pages/fridge/fridge',
+        icon: '/images/icons/fridge.png'
+      },
+      {
+        name: '吊顶木工',
+        url: '/pages/woods/woods',
+        icon: '/images/icons/woods.png'
+      }
+    ]
+  },
+  onLoad:function(){
+
+  }
+
+})
---
+++ pages/tiles/tiles.js
@@ -0,0 +1,38 @@
+//tiles.js
+
+const App = getApp()
+
+/*
+const db = wx.cloud.database({ env: 'siyangjiaju-7e0773' })
+var result = db.collection('windowsForStore').get().then(res => {
+  console.log(res.data)
+});
+*/
+var sliderWidth = 96; // 需要设置slider的宽度，用于计算中间位置
+
+
+Page({
+  data: {
+    tabs: ["品牌门店", "厂家直销"],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0,
+  },
+  onLoad: function () {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      }
+    });
+  },
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+  }
+});
